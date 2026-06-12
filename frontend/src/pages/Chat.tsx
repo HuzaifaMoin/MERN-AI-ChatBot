a06cf95858f18cddccb11d043b6f3391386713af
Huzaifa Moin <huzaifamoin784@gmail.com>
2026-06-12 23:55:17 +0500

    fix typeError

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -99,7 +99,6 @@ const Chat = () => {
               fontWeight: 700,
             }}
           >
-            {auth?.user?.name[0]}
             {auth?.user?.name?.split(" ")?.[1]?.[0] || auth?.user?.name?.[0]}          </Avatar>
           <Typography sx={{ mx: "auto", fontFamily: "work sans" }}>
             You are talking to a ChatBOT
